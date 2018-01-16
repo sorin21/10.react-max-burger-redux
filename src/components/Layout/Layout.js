a317ac6c49c023436fffaa28e13119f7b94dc625
sorin <dragonsorin@gmail.com>
2018-01-16 23:41:05 +0200

    Create the stateles components and stateful contaiers components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Aux from "../../hoc/Auxiliary";
+import classes from './Layout.css';
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {/* output the component we wrap this layout 
      so use this layout as wrapper around the core content
      that we want to render to the screen*/}
