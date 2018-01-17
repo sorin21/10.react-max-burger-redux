32ff7dc53180aeadb7c26ba89b724abd101338e9
Sorinel Dragomir <sorinel.dragomir@hub.vauban.ro>
2018-01-17 11:05:07 +0200

    Add message when is no ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,36 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  // gives you an array of the object keys
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      // make an array 
+      // console.log('igKey', igKey)
+      // console.log('props.ingredients', Object.keys(props.ingredients))
+      
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        // console.log('props.ingredients[igKey]', props.ingredients[igKey])
+        // salad + 2 for example
+        return <BurgerIngredient key={igKey + i} type={igKey} />
+      })
+      // use reduce to check if there is any element in the beginning
+      // take each element and add it to the array
+      // return arr.concat(el)
+      // the initial value or reduce value is an empty array
+      // .reduce((arr, el) => [...arr, ...el], []);
+      .reduce((arr, el) => {
+        console.log('arr', arr);
+        console.log('el', el);
+        return arr.concat(el)}, []);
+    });
+    console.log('transformedIngredients', transformedIngredients)
+    if(transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
