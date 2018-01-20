174165a1a016865f743c3d0c40449d5aa31df278
sorin <dragonsorin@gmail.com>
2018-01-20 15:04:27 +0200

    Add SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 import Aux from "../../hoc/Auxiliary";
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <main className={classes.Content}>
-      {/* output the component we wrap this layout 
-     so use this layout as wrapper around the core content
-     that we want to render to the screen*/}
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+  sideDrawerCloseHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
 
-export default layout;
+  sideDrawerToggleHandler = () => {
+    // seState() is asynchronous and to pass this.state.showSideDrawer 
+    // with no issue we have to use prevState
+    // switch between false and true
+    // prevState is passed by default
+    this.setState((prevState) => {  
+      return { showSideDrawer:  !prevState.showSideDrawer };
+    });
+  }
+
+  render() {
+    return <Aux>
+        <Toolbar drawerTooggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
+        <main className={classes.Content}>
+          {/* output the component we wrap this layout 
+        so use this layout as wrapper around the core content
+        that we want to render to the screen*/}
+          {this.props.children}
+        </main>
+      </Aux>;
+  }
+}
+
+export default Layout;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import classes from './NavigationItems.css'
-import NavigationItem from '../NavigationItem/NavigationItem';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
-  <ul className={classes.navigationItems}>
+  <ul className={classes.NavigationItems}>
     <NavigationItem
       active
       link="/">Burger Builder</NavigationItem>
